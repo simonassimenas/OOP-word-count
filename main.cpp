96e7f58b1407c009815fd1864fa760e3e43269e4
simonassimenas <simonas.simenas@gmail.com>
2023-05-12 16:29:32 +0300

    Finished program

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <regex>
+#include <locale>
 #include <map>
 #include <set>
 
@@ -10,80 +11,87 @@ using namespace std;
 
 int main() {
     // ==IO==
-    ifstream inputFile("input.txt");
+    locale::global(locale("lt_LT.UTF-8"));
+
+    wifstream inputFile("input.txt");
+    inputFile.imbue(locale());
     if (!inputFile) {
         cerr << "Error: Unable to open input file!" << endl;
         return 1;
     }
 
-    ofstream outputFile("output.txt", ios_base::out | ios_base::binary);
-	outputFile.imbue(locale("lt_LT.UTF-8"));
+    wofstream outputFile("output.txt", ios_base::out | ios_base::binary);
+    outputFile.imbue(locale());
     if (!outputFile) {
         cerr << "Error: Unable to open output file!" << endl;
         return 1;
     }
 
 
-    // ==URL==
-    string text((istreambuf_iterator<char>(inputFile)),
-                istreambuf_iterator<char>());
+    // ==URLS==
+    wstringstream textStream;
+    textStream << inputFile.rdbuf();
+    wstring text = textStream.str();
 
-    regex url_pattern("(https?://)?(www\\.)?[a-zA-Z0-9]+\\.[a-zA-Z]+(\\.[a-zA-Z]+)?(/[a-zA-Z0-9]*)*");
+    wregex url_pattern(L"(https?://)?(www\\.)?[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)*\\.[a-zA-Z]+(/[a-zA-Z0-9]*)*");
 
-    sregex_iterator it(text.begin(), text.end(), url_pattern);
-    sregex_iterator end;
+    wsregex_iterator it(text.begin(), text.end(), url_pattern);
+    wsregex_iterator end;
 
-    set<string> urls;
+    set<wstring> urls;
     for (; it != end; ++it) {
         urls.insert(it->str());
     }
-
-
-    // ==URL OUTPUT==
-    outputFile << "URL's in the presented text: " << "\n";
-    for (const auto& url : urls) {
-        outputFile << url << "\n";
-    }
-    outputFile << "\n";
-
     inputFile.seekg(0);
 
 
     // ==WORD COUNT==
-    map<string, int > wordCount;
-    map<string, set<int> > wordLines;
+    map<wstring, int > wordCount;
+    map<wstring, set<int> > wordLines;
     int lineNum = 0;
-    string line;
+    wstring line;
 
     while (getline(inputFile, line)) {
         ++lineNum;
-        stringstream ss(line);
-        string word;
+        wstringstream ss(line);
+        wstring word;
         
         while (ss >> word) {
-            // reikia padaryti kad neimtu simboliu bet paimtu lietuviskas raides
-            while (!word.empty() && !isalpha(word[0], locale("lt_LT.UTF-8"))) {
+            while (!word.empty() && !isalpha(word[0], locale())) {
                 word.erase(0, 1);
             }
-            while (!word.empty() && !isalpha(word[word.length() - 1], locale("lt_LT.UTF-8"))) {
+            while (!word.empty() && !isalpha(word[word.length() - 1], locale())) {
                 word.erase(word.length() - 1, 1);
             }
 
-            wordCount[word]++;
+            if(!word.empty()) {
+                wordCount[word]++;
+            }
             wordLines[word].insert(lineNum);
         }
     }
 
 
-    // ==CROSS-REFERENCE TABLE OUTPUT==
-    outputFile << "Cross-reference table:\n" <<
+    // ==URL OUTPUT==
+    outputFile << "URL's in the presented text: \n\n";
+
+    for (const auto& url : urls) {
+        outputFile << url << "\n";
+    }
+
+
+    // ==CROSS-REFERENCE TABLE OUTPUT
+    outputFile << "\n\nCross-reference table:\n"
+                << "\n" <<
                 setw(30) << fixed << left << "Word" <<
                 setw(10) << fixed << left << "Count" <<
-                " References\n" << string(53, '-') << "\n";
+                " References\n" << wstring(27, '-') <<
+                wstring(3, ' ') << wstring(8, '-') <<
+                wstring(3, ' ') << wstring(60, '-') << "\n";
 
     for (const auto& word : wordCount) {
         if (word.second > 1) {
-            string wordCapitalized = word.first;
+            wstring wordCapitalized = word.first;
             wordCapitalized[0] = toupper(wordCapitalized[0]);
 
             outputFile << setw(30) << fixed << left << wordCapitalized <<
